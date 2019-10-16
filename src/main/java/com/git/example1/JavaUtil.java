a91373856bca71ba807ab5b1ef19a659055f3b50
Keerthana <Keerthana@Apples-MacBook-Pro.local>
2019-10-16 16:36:21 +0530

    Task 1 Completed

--- src/main/java/com/git/example1/JavaUtil.java
+++ src/main/java/com/git/example1/JavaUtil.java
@@ -3,7 +3,9 @@ package com.git.example1;
 public class JavaUtil {
 	
 	public static void main(String args[]) {
-     System.out.println("Hello11");
+System.out.prinltn(“taks1 changes Raghu”);
+     System.out.println("Hello11-TESK1 RAGHU”);
+System.out.prinltn(“taks1 changes Raghu”);
      
 	}
 	
