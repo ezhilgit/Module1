009d3ad8922ffee055a8e217844fb2f9a9c3c4f5
Santhan <ezhilgit@gmail.com>
2019-10-16 16:59:52 +0530

    Modified JavaUtil

--- src/main/java/com/git/example1/JavaUtil.java
+++ src/main/java/com/git/example1/JavaUtil.java
@@ -3,7 +3,7 @@ package com.git.example1;
 public class JavaUtil {
 	
 	public static void main(String args[]) {
-     System.out.println("Hello11");
+     System.out.println("Hello11-Task2");
      
 	}
 	
