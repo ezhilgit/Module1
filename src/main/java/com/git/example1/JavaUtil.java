01603b7fcbf976e7c448e34a864e6584e54d8b4c
Santhan <ezhilgit@gmail.com>
2019-11-01 19:53:49 +0530

    Changed

--- src/main/java/com/git/example1/JavaUtil.java
+++ src/main/java/com/git/example1/JavaUtil.java
@@ -4,7 +4,7 @@ public class JavaUtil {
 	
 	public static void main(String args[]) {
 System.out.println("taks1 changes Raghu");
-     System.out.println("Hello11-TESK1 RAGHU");
+     System.out.prinln("Hello11-TESK1 RAGHU");
 System.out.println("taks1 changes Raghu");
 
      
